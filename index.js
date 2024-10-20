b5fb16296d60673b5487759de304e78efa095037
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2024-10-20 21:00:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;
 //====节点参数
-const uuid = process.env.UUID || 'fd80f56e-93f3-4c85-b2a8-c77216c509a7'; //不填，则使用随机
+const uuid = process.env.UUID || '31f08bb9-99fa-4948-8268-1b2ab12033a7'; //不填，则使用随机
 const vmms = process.env.MPATH || 'vms'; //vms path
 const vmmport = process.env.VM_PORT || '8001';//vms 端口
 const vpath = process.env.VPATH || 'vls';//vls path
